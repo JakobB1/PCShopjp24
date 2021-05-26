873f23e332236192637738e54b6536f0f3c02df9
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-05-26 14:32:19 +0100

    createTables

--- PCShopjp24.sql
+++ PCShopjp24.sql
@@ -2,4 +2,44 @@ drop database if exists PCShopjp24;
 create database PCShopjp24;
 use PCShopjp24;
 
-create table
+create table vlasnik(
+  sifra int not null primary key auto_increment,
+  ime varchar(50) not null,
+  prezime varchar(50) not null,
+  iban varchar(50),
+  email varchar(50) not null,
+  PCshop int 
+);
+
+create table PCshop(
+  sifra int not null primary key auto_increment,
+  naziv varchar(30) not null,
+  oib varchar(40) not null,
+  djelatnik int
+);
+
+create table djelatnik(
+  sifra int not null primary key auto_increment,
+  ime varchar(50) not null,
+  prezime varchar(50) not null,
+  proizvod int 
+);
+
+create table proizvod(
+  sifra int not null primary key auto_increment,
+  naziv varchar(30) not null,
+  cijena decimal(12,10) not null,
+  garancija datetime not null
+);
+
+create table korisnik(
+  sifra int not null primary key auto_increment,
+  ime varchar(50) not null,
+  prezime varchar(50) not null
+);
+
+create table usluga(
+  sifra int not null primary key auto_increment,
+  djelatnik int not null,
+  korisnik int not null
+);
