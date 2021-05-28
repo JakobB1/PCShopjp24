67d1ab1af1297fa190151fa2a0700141af1ba95d
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-05-28 15:37:21 +0100

    updated

--- PCShopjp24.sql
+++ PCShopjp24.sql
@@ -2,110 +2,89 @@ drop database if exists PCShopjp24;
 create database PCShopjp24;
 use PCShopjp24;
 
-create table vlasnik(
-  sifra int not null primary key auto_increment,
-  ime varchar(50) not null,
-  prezime varchar(50) not null,
-  iban varchar(50),
-  email varchar(50) not null,
-  PCshop int 
-);
-
-create table PCshop(
+create table pcshop(
   sifra int not null primary key auto_increment,
   naziv varchar(30) not null,
-  oib varchar(40) not null,
-  djelatnik int not null
+  oib char(11),
+  vlasnik int
 );
 
 create table djelatnik(
   sifra int not null primary key auto_increment,
   ime varchar(50) not null,
   prezime varchar(50) not null,
-  proizvod int 
+  pcshop int not null
+);
+
+create table racun(
+  sifra int not null primary key auto_increment,
+  datumpocetka datetime not null,
+  brojracuna varchar(100) not null,
+  korisnik int not null,
+  djelatnik int not null
 );
 
 create table proizvod(
   sifra int not null primary key auto_increment,
   naziv varchar(30) not null,
-  cijena decimal(14,6) not null,
+  cijena decimal(12,8) not null,
   garancija datetime not null
+);
+
+create table stavka(
+  sifra int not null primary key auto_increment,
+  proizvod int not null,
+  racun int not null
 );
 
 create table korisnik(
   sifra int not null primary key auto_increment,
-  ime varchar(50) not null,
-  prezime varchar(50) not null
+  ime varchar(30) not null,
+  prezime varchar(30) not null
 );
 
-create table usluga(
-  sifra int not null primary key auto_increment,
-  djelatnik int not null,
-  korisnik int not null
-);
+alter table djelatnik add foreign key(pcshop) references pcshop(sifra);
+alter table racun add foreign key(korisnik) references korisnik(sifra);
+alter table racun add foreign key(djelatnik) references djelatnik(sifra);
+alter table stavka add foreign key(proizvod) references proizvod(sifra);
+alter table stavka add foreign key(racun) references racun(sifra);
 
-alter table vlasnik add foreign key(PCshop) references PCshop(sifra);
-alter table PCshop add foreign key(djelatnik) references djelatnik(sifra);
-alter table djelatnik add foreign key(proizvod) references proizvod(sifra);
+select * from pcshop;
+insert into pcshop(naziv)
+values  ('Mikronis');
 
-alter table usluga add foreign key(djelatnik) references djelatnik(sifra);
-alter table usluga add foreign key(korisnik) references korisnik(sifra);
-
-select * from proizvod;
-insert into proizvod(naziv,cijena,garancija)
-values  ('Nvidia graficka',1999.99,'2025-03-23'),
-        ('AMD graficka',1599.99,'2026-04-13'),
-        ('Razer mis',599.99,'2027-11-03'),
-        ('Logitech mis',499.99,'2023-01-30'),
-        ('Corsair tipkovnika',1199.99,'2024-12-03');
-        
 select * from djelatnik;
-insert into djelatnik(ime,prezime,proizvod)
+insert into djelatnik(ime,prezime,pcshop)
 values  ('Marko','Markovic',1),
-        ('Ivo','Ivic',2),
-        ('Pero','Peric',3),
-        ('Mia','Mijic',4),
-        ('Karlo','Karlovic',5);
-        
-select * from PCshop;
-insert into PCshop(naziv,oib,djelatnik)
-values  ('Links',12345678910,1),
-        ('Instar',12345678911,2),
-        ('Mikronis',12345678912,3),
-        ('HGshop',12345678913,4),
-        ('FuturaIT',12345678914,5);
-        
-select * from vlasnik;
-insert into vlasnik(ime,prezime,email,PCshop)
-values  ('Tin','Tinic','tin@gmail.com',1),
-        ('Maja','Majic','maja@gmail.com',2),
-        ('Iva','Ivic','iva@gmail.com',3),
-        ('Tena','Tenic','tena@gmail.com',4),
-        ('Matea','mateic','mate@gmail.com',5);
+        ('Pero','Peric',1),
+        ('Ivo','Ivic',1);
         
 select * from korisnik;
 insert into korisnik(ime,prezime)
-values  ('Marko','Markovic'),
-        ('Frano','Franjic'),
-        ('Marina','Marinic'),
-        ('Ivan','Ivanovic'),
-        ('Darko','Darkovic');
+values  ('Maja','Majic'),
+        ('Iva','Ivic'),
+        ('Petra','Petrovic');
         
-select * from usluga;
-insert into usluga(djelatnik,korisnik)
-values  (1,5),(2,4),(3,3),(4,2),(5,1);
-
-
-select ime from korisnik where ime like '%Marko%';
-select ime from korisnik where ime like '%an';
-select naziv from PCshop where naziv like '%Links%' and djelatnik=1;
-select naziv from PCshop where djelatnik=1 is not null;
-select garancija from proizvod where garancija like '%2025%';
-select garancija from proizvod where garancija like '%2024%';
-
-update proizvod set garancija = '2030-03-23' where sifra=4;
-update proizvod set garancija = '2040-12-23' where sifra=1;
-update korisnik set ime = 'Luka' where sifra =1;
-update korisnik set prezime = 'Lukovic' where sifra =1;
-update proizvod set naziv = 'NZXT kuciste' where sifra=1;
-update PCshop set naziv = 'PCshop' where sifra=1;
+select * from racun;
+insert into racun(datumpocetka,brojracuna,korisnik,djelatnik)
+values  ('2012-06-21',123,1,3),
+        ('2013-01-30',231,2,2),
+        ('2014-02-27',321,3,1);
+        
+select * from proizvod;
+insert into proizvod(naziv,cijena,garancija)
+values  ('Razer tipkovnica',1099.99,'2025-03-03'),
+        ('Razer mis',599.99,'2027-12-05'),
+        ('Logitech tipkovnica',899.99,'2028-02-09'),
+        ('Logitech mis',499.99,'2026-10-06'),
+        ('Corsair mis',449.99,'2026-11-07'),
+        ('Corsair tipkovnica',1199.99,'2026-12-23');
+        
+select * from stavka;
+insert into stavka(proizvod,racun)
+values  (1,1),
+        (2,1),
+        (3,2),
+        (4,2),
+        (5,3),
+        (6,3);
